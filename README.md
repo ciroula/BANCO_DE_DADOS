230c4abaac89630132d916a3b87f91e32aceb9da
ciroula <159503125+ciroula@users.noreply.github.com>
2024-10-14 12:32:28 +0000

    base de bancos

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # BANCO_DE_DADOS
+#Aula de CRUD - Banco de Dados
+
+## Objetivo:
+criar conexao com Banco de Dados  e realizar operacoes basicas(CRUD)
+
+### Tecnologia:
+- ORM: SQLAlchemy
+- Banco de dados: SQLite
+
+#### Detalhes:
+craindo conexao com banco de dados SQLite, usando SQLAlchemy como ferramenta ORM para
+interacao com banco de dados
---
+++ main.py
@@ -0,0 +1,69 @@
+"""
+    BANCO DE DADOS
+    - SQL (LINGUAGEM DE CONSULTAS ESTRUTURADAS)
+    - EXEMPLO:
+        - SELECT * FROM CLIENTES;
+        - IRA CONSULTAR O BANCO DE DADOS NA TABELA CLIENTES.
+
+        SGDB:
+            - GERENCIAR PERMISSOES DE ACESSO
+            - ADMINISTRADOR DE BANCO DE DADOS (BDA)
+            - CRIAR CONSULTAS PERSONALIZADAS 
+            - SELECT * FROM CLIENTES;
+    - ORM: MAPEAMENTO OBJETO RELACIONAL
+        - USAR A LIGUAGEM DE PROGRAMACAO PARA
+          MANIPULAR O BANCO DE DADOS.
+    - INSTALANDO ORM PARA PYTHON:
+        - pip install sqlalchemy
+"""
+
+import os 
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+#criando banco de dados
+MEU_BANCO = create_engine("sqlite:///meubanco.db")
+
+#criando conexao com banco de dados.
+Session = sessionmaker(bind=MEU_BANCO)
+session = Session()
+
+# criando tabela.
+Base = declarative_base()
+
+class Cliente(Base):
+    __tablename__ = "clientes"
+
+    #definindo campos da tabela
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome",String)
+    email = Column("email",String)
+    senha = Column("senha",String)
+
+    def __init__(self, nome: str, email: str, senha: str):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+
+#criando atributos de classe
+Base.metadata.create_all(bind=MEU_BANCO)
+
+#CRUD
+#Create - Insert - Salvar
+os.system("cls || clear")
+print("soicitando dados para o usuario")
+inserir_nome = input("Digite o seu nome: ")
+inserir_email = input("Digite o seu email: ")
+inserir_senha = input("Digite a sua senha: ")
+
+cliente = Cliente(nome=inserir_nome, email=inserir_email, senha=inserir_senha)
+session.add(cliente)
+session.commit()
+
+#Read - Select - Consultas
+print("\nExibindo dados de todos os clientes.")
+lista_clientes = session.query(Cliente).all()
+
+for cliente in lista_clientes:
+    print(f"{cliente.id} - {cliente.nome} - {cliente.email} - {cliente.senha}")
+
